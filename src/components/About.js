f86dc0b3fb57ec5f5eee36b6c133081e3a63aebb
elisemcclain <celisemcclain@gmail.com>
2023-07-02 19:59:18 -0500

    hooks props

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  const { bio } = props;
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{bio ? props.bio.textContent : null}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>
+        Links
+        <a href="https://github.com/liza">https://github.com/liza</a>
+        <a href="https://www.linkedin.com/in/liza/">
+          https://www.linkedin.com/in/liza/
+        </a>
+      </h3>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
+  // console.log("fromApp", user);
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar props={user} />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} />
+      <nav> links={user.links}</nav>
     </div>
   );
 }
